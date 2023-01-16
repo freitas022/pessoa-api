dbc19b50c259636b76641bee7b7d7ad502f13f34
Matheus Freitas <84873099+freitas022@users.noreply.github.com>
2023-01-16 09:52:38 -0300

    Commit inicial

---
+++ src/test/java/com/backend/pessoaapi/PessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pessoaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
